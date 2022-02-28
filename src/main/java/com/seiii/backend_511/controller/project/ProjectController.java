2ca4854b286f639b9e313d8bd0f651da7b7f0f26
skyswords7 <710487762@qq.com>
2022-02-28 13:02:19 +0800

    添加部分todo

--- src/main/java/com/seiii/backend_511/controller/project/ProjectController.java
+++ src/main/java/com/seiii/backend_511/controller/project/ProjectController.java
@@ -1,9 +1,11 @@
 package com.seiii.backend_511.controller.project;
 
+import com.github.pagehelper.PageInfo;
 import com.seiii.backend_511.po.project.Project;
 import com.seiii.backend_511.service.project.ProjectService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.seiii.backend_511.vo.ResultVO;
+import com.seiii.backend_511.vo.project.ProjectVO;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -12,4 +14,35 @@ import javax.annotation.Resource;
 public class ProjectController {
     @Resource
     private ProjectService projectservice;
+
+    @PostMapping("/create")
+    public ResultVO<ProjectVO> createProject(@RequestBody ProjectVO project){
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/active/{page}")
+    public PageInfo<ProjectVO> getActiveProjects(){
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/all/{page}")
+    public PageInfo<ProjectVO> getAllProjects(){
+        //TODO
+        return null;
+    }
+
+    //TODO mapping未设置
+    @GetMapping("/")
+    public PageInfo<ProjectVO> getProjectsByUserId(){
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/{projectId}")
+    public ProjectVO getProjectById(@PathVariable Integer projectId){
+        //TODO
+        return null;
+    }
 }
