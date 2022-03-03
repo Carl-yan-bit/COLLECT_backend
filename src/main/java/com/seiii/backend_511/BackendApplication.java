a1c7feb4be20a266e3fe80cca79d0beaa712213c
skyswords7 <710487762@qq.com>
2022-03-03 22:46:37 +0800

    尝试获取服务器jar包路径

--- src/main/java/com/seiii/backend_511/BackendApplication.java
+++ src/main/java/com/seiii/backend_511/BackendApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         SpringApplication.run(BackendApplication.class, args);
+        String LOCATION = URLDecoder.decode(BackendApplication.class.getProtectionDomain().getCodeSource().getLocation().getFile(),
+                "UTF-8");
+        System.out.println("获取路径成功：LOCATION=" + LOCATION);
     }
 
 }
