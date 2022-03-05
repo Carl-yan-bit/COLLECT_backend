71ba3dba47917c5f967cf7ae7bbb3f08cb61349c
skyswords7 <710487762@qq.com>
2022-03-05 14:04:03 +0800

    部署相关文件修改

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 containerId=`docker ps -a | grep -w backend_511 | awk '{print $1}'`
 if [ "$containerId" != "" ];then
-	docker stop backend
-	docker rm backend
+	docker stop backend_511
+	docker rm backend_511
 fi
