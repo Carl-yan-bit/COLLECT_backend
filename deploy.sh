9de76e7f7b0cb14bf8b1eae77bf32a0e3237ac4a
skyswords7 <710487762@qq.com>
2022-03-10 21:21:56 +0800

    部署修改

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+imageId=`docker images | grep -w backend_511 | awk '{print $3}'`
+if [ "$imageId" != "" ];then
+	docker rmi -f backend_511
+fi
 containerId=`docker ps -a | grep -w backend_511 | awk '{print $1}'`
 if [ "$containerId" != "" ];then
 	docker stop backend_511
