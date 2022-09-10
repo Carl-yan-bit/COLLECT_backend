6b684650a8e709dfea013b35cdd357d455479989
191250171 <191250171@smail.nju.edu.cn>
2022-09-10 15:36:54 +0800

    Restful风格重构

--- src/main/java/com/seiii/backend_511/controller/user/UserController.java
+++ src/main/java/com/seiii/backend_511/controller/user/UserController.java
@@ -16,22 +16,46 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @PostMapping("/register")
+    /**
+     * POST: 用户注册
+     * @param userVO
+     * @return
+     */
+    @PostMapping("")
     public ResultVO<UserVO> register(@RequestBody UserVO userVO){
         return userService.userRegister(userVO);
     }
+
+    /**
+     * POST: 用户登录
+     * @param userFormVo
+     * @return
+     */
     @PostMapping("/login")
     public ResultVO<UserVO> login(@RequestBody UserFormVo userFormVo){
         return userService.userLogin(userFormVo.getUser_idx(),userFormVo.getPassword());
     }
-    @PostMapping("/changePassword")
+
+    /**
+     * 更改密码
+     * @param userFormVo
+     * @return
+     */
+    @PostMapping("/password")
     public ResultVO<UserVO> changePassword(@RequestBody UserFormVo userFormVo){
         return userService.userChangePassword(userFormVo.getUser_idx(), userFormVo.getPassword(), userFormVo.getPassword_new());
     }
+
+    /**
+     * GET: 根据用户ID获取用户信息
+     * @param uid
+     * @return 用户信息
+     */
     @GetMapping("/find/{uid}")
     public ResultVO<UserVO> getUserByUid(@PathVariable Integer uid){
         return userService.getUserByUidWithCode(uid);
     }
+
     @PostMapping("/device/add")
     public ResultVO<List<DeviceVO>> addUserDevice(@RequestBody UserDeviceListVO userDeviceVOList){
         return userService.addUserDevice(userDeviceVOList);
@@ -44,6 +68,7 @@ public class UserController {
     public ResultVO<List<Device>> getUserDevices(@RequestParam Integer uid){
         return userService.getUserDevices(uid);
     }
+
     @GetMapping("/attribute")
     public ResultVO<UserAttributeVO> getUserAttribute(@RequestParam Integer uid){
         return userService.getUserAttribute(uid);
