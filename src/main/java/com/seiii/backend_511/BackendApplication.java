66e9b3f11c9ed5c72bd3cef37816a478a6caa174
191250171 <191250171@smail.nju.edu.cn>
2022-09-14 17:22:10 +0800

    springcloud

--- src/main/java/com/seiii/backend_511/BackendApplication.java
+++ src/main/java/com/seiii/backend_511/BackendApplication.java
@@ -3,11 +3,15 @@ package com.seiii.backend_511;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class BackendApplication {
 
