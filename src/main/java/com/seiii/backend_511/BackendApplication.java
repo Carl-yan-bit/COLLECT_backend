4c3e241d7b39c91a9017b4833f380b084080d72e
191250171 <191250171@smail.nju.edu.cn>
2022-09-15 00:14:51 +0800

    配置jar包路径:

--- src/main/java/com/seiii/backend_511/BackendApplication.java
+++ src/main/java/com/seiii/backend_511/BackendApplication.java
@@ -1,11 +1,6 @@
 package com.seiii.backend_511;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
